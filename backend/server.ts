6f34c49031844e91352ca6e82694bebbac4779ca
Jahakeme <jucalasitok@gmail.com>
2025-10-30 15:07:44 +0100

    Changes to tsconfig.json

--- backend/server.ts
+++ backend/server.ts
@@ -2,7 +2,7 @@
 import express from 'express';
 import { Server } from 'socket.io';
 import http from 'http';
-import prisma from './prisma/connection';
+import prisma from './prisma/connection.js';
 
 
 interface ClientMessage {
